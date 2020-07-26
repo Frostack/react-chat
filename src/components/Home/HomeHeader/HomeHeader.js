57a82d56e1a5d9cd09d678410f4bc85a844e90e9
Frostack <soulburn007@gmail.com>
2020-07-26 21:39:43 +0430

    🔗 add channels link

--- src/components/Home/HomeHeader/HomeHeader.js
+++ src/components/Home/HomeHeader/HomeHeader.js
@@ -9,7 +9,7 @@ function HomeHeader() {
       <h1 className="display-3">ReactJS Chat</h1>
       <p className="lead">Real-time Chat Application Powered with React and React-bootstrap</p>
       <p>For more information visit website</p>
-      <Link to="/easy">
+      <Link to="/chat/general">
         <Button variant="info">More Info</Button>
       </Link>
     </Jumbotron>
